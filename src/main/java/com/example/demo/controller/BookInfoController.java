28d009e648b42f2f6d6b69c02863928644c2f8ed
silkwave <silkwave24@gmail.com>
2025-01-08 23:09:55 +0900

    html

--- src/main/java/com/example/demo/controller/BookInfoController.java
+++ src/main/java/com/example/demo/controller/BookInfoController.java
@@ -5,8 +5,13 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.service.BookInfoServiceImpl;
 import com.example.demo.vo.BookInfoVO;
