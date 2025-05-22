3da6977eb85ac379cee1d6df2559814d48219022
silkwave <silkwave24@gmail.com>
2025-05-22 22:42:26 +0900

    RedisClientApp

--- src/main/java/com/example/demo/vo/BookInfoVO.java
+++ src/main/java/com/example/demo/vo/BookInfoVO.java
@@ -1,12 +1,8 @@
 package com.example.demo.vo;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.Data;
  
-@Getter
-@Setter
-@ToString
+@Data
 public class BookInfoVO {
 
    private int book_key;
